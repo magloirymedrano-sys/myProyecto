860cd6dad29a1f98ddcf596e0143c9d2529a4f13
Magloiry Medrano <magloirymedrano@gmail.com>
2026-05-17 13:42:03 -0400

    tituto

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ function App() {
 
   return (
     <>
+      <h1>Mi App</h1>
      <Formularios/>
      <Cuadro url="https://empresas.blogthinkbig.com/wp-content/uploads/2019/11/Imagen3-245003649.jpg?w=800" texto="Imagen de ejemplo" />
 
